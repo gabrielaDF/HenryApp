28b8bf6e7688deae114c5f3a41b948f44b7452af
gabrielaDF <defreitasgabi@gmail.com>
2024-02-28 13:01:03 -0500

    inicializacion de la app

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./assets/favicon.png")} />
+      <TextInput
+        placeholder="Type here to capitalize"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
+      <Text>{text.toUpperCase()}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
